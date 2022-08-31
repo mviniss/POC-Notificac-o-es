0f8d9ce694dcab80eb3011da0a72209be1c143ac
mviniss <m_vinicius.ss@icloud.com>
2022-08-31 18:00:11 -0300

    Update ContentView.swift
    POC de notificações com time trigger

--- POC Notificações/ContentView.swift
+++ POC Notificações/ContentView.swift
@@ -6,11 +6,61 @@
 //
 
 import SwiftUI
+import NotificationCenter
+
+class NotificationManager {
+    
+    static let instance = NotificationManager()
+    
+    func requestAuthorization(){
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { (success, error) in
+            if let error = error {
+                print ("Error: \(error)")
+                
+            } else {
+                print ("Success")
+            }
+        }
+    }
+    
+    func scheduleNotification() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Essa é a nossa primeira notificação"
+        content.subtitle = "Pedro Henrique é levado"
+        content.sound = .default
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString,
+                                            content: content,
+                                            trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request)
+    }
+    
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 40){
+            Button("Permissão") {
+                
+                NotificationManager.instance.requestAuthorization()
+            }
+            
+            Button("Notificação") {
+                
+                NotificationManager.instance.scheduleNotification()
+            }
+            
+        }
+        .onAppear{
+            UIApplication.shared.applicationIconBadgeNumber = 0
+        }
     }
 }
 
