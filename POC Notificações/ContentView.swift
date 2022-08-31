1639987015318813096ad4491ad64b0ac84f4050
mviniss <m_vinicius.ss@icloud.com>
2022-08-31 16:46:01 -0300

    Initial Commit

---
+++ POC Notificações/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  POC Notificações
+//
+//  Created by Marcus Vinicius Silva de Sousa on 31/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ POC Notificações/POC_Notificac_o_esApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_Notificac_o_esApp.swift
+//  POC Notificações
+//
+//  Created by Marcus Vinicius Silva de Sousa on 31/08/22.
+//
+
+import SwiftUI
+
+@main
+struct POC_Notificac_o_esApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
